813f2aca6693b5c74fc06eb77fb2afc2f675c1b1
Wasin Sombatboon <59160383@my.buu.ac.th>
2019-10-16 17:58:44 +0700

    second commit

--- app/src/main/java/com/example/calcalculated/mainFragment.kt
+++ app/src/main/java/com/example/calcalculated/mainFragment.kt
@@ -2,13 +2,12 @@ package com.example.calcalculated
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.calcalculated.databinding.FragmentMainBinding
 
 
@@ -23,8 +22,17 @@ class mainFragment : Fragment() {
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentMainBinding>(inflater,R.layout.fragment_main,container,false)
 
-
+        setHasOptionsMenu(true)
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.option_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+            view!!.findNavController())
+                || super.onOptionsItemSelected(item)
+    }
 }
